8b352f67efd7aa2b1c40cae7a8352c63390e8b79
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2020-03-11 15:03:08 +0100

    knowrepo script now works from any dir

--- knowrepo.sh
+++ knowrepo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# author: Rasmus Olofzon, 2020-03-02
+# author: Rasmus Olofzon, 2020-03-03
 # https://github.com/razofz/helper-scripts 
 
 firefox --new-window &
-nohup markserv \
-	../../../../../../../../../../../../../../../../../${NOTES_DIR}/knowledge-repo \
-	-p 8642 &
 sleep 2
-vim ${NOTES_DIR}/knowledge-repo/knowledge-repo.md
+nohup markserv \
+	../../../../../../../../../../../../../../../../..${NOTES_DIR}/knowledge-repo \
+	-p 8642 &
+vim ../../../../../../../../../../../../../../../../..${NOTES_DIR}/knowledge-repo/knowledge-repo.md
